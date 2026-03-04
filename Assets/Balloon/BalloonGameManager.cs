739fd7dcdbe6711a2a195a3aa505a9c4f59e71dd
Yu-Da-Shen <1625465428@qq.com>
2026-03-04 17:43:29 +0800

    rr
    r

--- Assets/Balloon/BalloonGameManager.cs
+++ Assets/Balloon/BalloonGameManager.cs
@@ -10,6 +10,11 @@ public class BalloonGameManager : MonoBehaviour
     public AnimationClip ccb;
     public float shortPressAmount = 0.2f;
     public float continuousSpeed = 0.8f;
+
+    [Header("漏气设置")]
+    public float deflateSpeed = 0.4f;
+    public Vector3 minScale;
+
     [SerializeField] private float maxHoldTime;
     [SerializeField] private TextMeshProUGUI gameTip;
     private float currentHoldTimer = 0f;
@@ -22,6 +27,7 @@ public class BalloonGameManager : MonoBehaviour
         if (instance == null) instance = this;
         maxHoldTime = Random.Range(4.0f, 8.0f);
         anim = balloonPrefab.GetComponent<Animator>();
+        minScale = balloonPrefab.transform.localScale;
     }
 
     private void Update()
@@ -34,9 +40,14 @@ public class BalloonGameManager : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.Space) && canContinuousInflate)
         {
-            gameTip.text = $"持续充气中... 持续时间: {currentHoldTimer:F1}s";
+            gameTip.text = $"持续充气中... 进度: {currentHoldTimer:F1}s";
             HandleContinuousInflation();
         }
+        else if (!Input.GetKey(KeyCode.Space))
+        {
+            HandleDeflation();
+        }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             StopInflating();
@@ -48,6 +59,7 @@ public class BalloonGameManager : MonoBehaviour
         anim.Play("充充爆", 0, 0f);
         float waitTime = (ccb != null) ? ccb.length : 0.3f;
         yield return new WaitForSeconds(waitTime);
+
         if (Input.GetKey(KeyCode.Space))
         {
             canContinuousInflate = true;
@@ -57,6 +69,7 @@ public class BalloonGameManager : MonoBehaviour
             anim.Play("Idle");
         }
     }
+
     private void HandleContinuousInflation()
     {
         currentHoldTimer += Time.deltaTime;
@@ -68,11 +81,40 @@ public class BalloonGameManager : MonoBehaviour
             Explode();
         }
     }
+    private void HandleDeflation()
+    {
+        if (balloonPrefab.transform.localScale.x > minScale.x)
+        {
+            float shrink = deflateSpeed * Time.deltaTime;
+            balloonPrefab.transform.localScale -= new Vector3(shrink, shrink, shrink);
+            currentHoldTimer -= Time.deltaTime * (deflateSpeed / continuousSpeed);
+            if (currentHoldTimer < 0) currentHoldTimer = 0;
+
+            gameTip.text = "气球漏气中...";
+        }
+        else
+        {
+            balloonPrefab.transform.localScale = minScale;
+            currentHoldTimer = 0;
+        }
+    }
 
     private void StopInflating()
     {
         canContinuousInflate = false;
         if (prepareRoutine != null) StopCoroutine(prepareRoutine);
+
+        if (!isGameOver)
+        {
+            if (balloonPrefab.transform.localScale.x >= minScale.x)
+            {
+                anim.Play("爆爆充");
+            }
+            else
+            anim.Play("Idle");
+        }
+        
+
         Debug.Log("停止充气，当前计时：" + currentHoldTimer);
     }
 
